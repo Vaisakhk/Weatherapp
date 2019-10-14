2492de10a0a614a083e9bba7bfb694f594a77dd9
VK <Vaisakh Krishnan>
2019-10-14 22:57:03 +0800

    Scene Delegate Classes Removed

--- Weather App/AppDelegates/AppDelegate.swift
+++ Weather App/AppDelegates/AppDelegate.swift
@@ -19,16 +19,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
     return true
     }
-
-    // MARK: UISceneSession Lifecycle
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    }
-
 }
 
